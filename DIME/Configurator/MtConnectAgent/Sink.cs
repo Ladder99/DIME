a70188924a28c23343be13495a1109a3c5328fd9
MRIIOT <cmisztur@mriiot.com>
2024-12-29 11:36:38 -0600

    MTC

--- DIME/Configurator/MtConnectAgent/Sink.cs
+++ DIME/Configurator/MtConnectAgent/Sink.cs
@@ -11,9 +11,6 @@ public static class Sink
             .MakeConfig(section, "MTConnectAgent", Configuration.ConnectorDirectionEnum.Sink);
 
         config.Port = section.ContainsKey("port") ? Convert.ToInt32(section["port"]) : 5000;
-        config.DeviceUuid = section.ContainsKey("device_uuid") ? Convert.ToString(section["device_uuid"]) : Guid.NewGuid().ToString();
-        config.DeviceId = section.ContainsKey("device_id") ? Convert.ToString(section["device_id"]) : Guid.NewGuid().ToString();
-        config.DeviceName = section.ContainsKey("device_name") ? Convert.ToString(section["device_name"]) : Guid.NewGuid().ToString();
         
         var connector = new Connectors.MtConnectAgent.Sink(config, disruptor);
 
--- DIME/Connectors/MtConnectAgent/Sink.cs
+++ DIME/Connectors/MtConnectAgent/Sink.cs
@@ -14,7 +14,6 @@ namespace DIME.Connectors.MtConnectAgent;
 public class Sink: SinkConnector<ConnectorConfiguration, ConnectorItem>
 {
     private MTConnectAgentApplication _client = null;
-    private Module _module = null;
     
     public Sink(ConnectorConfiguration configuration, Disruptor.Dsl.Disruptor<MessageBoxMessage> disruptor) : base(configuration, disruptor)
     {
@@ -33,35 +32,53 @@ public class Sink: SinkConnector<ConnectorConfiguration, ConnectorItem>
 
     protected override bool ConnectImplementation()
     {
-        //_client.StartAgent(new AgentApplicationConfiguration() { });
-        _client.Run(["run", "MtConnectAgentSink.yaml"], false);
-        _module = new Module(_client.Agent, this);
-        _module.Start();
+        _client.StartAgent(new AgentApplicationConfiguration() { 
+            Modules = new []
+            {
+                new Dictionary<object, object> ()
+                {
+                    {"http-server", new Dictionary<object, object>()
+                    {
+                        {"port", Configuration.Port},
+                    }}
+                }
+            }
+        });
+        
         IsConnected = true;
         return true;
     }
 
     protected override bool WriteImplementation()
     {
-        return true;
-    }
+        foreach (var message in Outbox)
+        {
+            if (message.ConnectorItemRef is not null &&
+                message.ConnectorItemRef.SinkMeta is not null &&
+                message.ConnectorItemRef.SinkMeta.ContainsKey("mtconnect"))
+            {
+                var devices = _client.Agent.GetDevices().ToDictionary(o => o.Name, o => (Device)o);
+                
+                var (wasModified, device, dataItem) = 
+                    Builder.Build(
+                        devices, 
+                        message.ConnectorItemRef.SinkMeta["mtconnect"].ToString(), 
+                        message.Path);
 
-    public override bool AfterUpdate()
-    {
-        Logger.Trace($"[{Configuration.Name}] SinkConnector:AfterUpdate::ENTER");
-        
-        // Do not clear outbox here.  That is the module's job.
-
-        IsWriting = false;
-        
-        Logger.Trace($"[{Configuration.Name}] SinkConnector:AfterUpdate::EXIT");
+                if (wasModified)
+                {
+                    _client.Agent.AddDevice(device);
+                }
+                
+                _client.Agent.AddObservation(device.Uuid, dataItem.Id, Configuration.UseSinkTransform ? TransformAndSerializeMessage(message) : message.Data, message.Timestamp);
+            }
+        }
         
         return true;
     }
 
     protected override bool DisconnectImplementation()
     {
-        _module.Stop();
         _client.StopAgent();
         IsConnected = false;
         return true;
@@ -70,50 +87,5 @@ public class Sink: SinkConnector<ConnectorConfiguration, ConnectorItem>
     protected override bool DeinitializeImplementation()
     {
         return true;
-    }
-}
-
-public class Module : MTConnectInputAgentModule
-{
-    public const string ConfigurationTypeId = "dime-sink";
-    Sink _connector = null;
-    
-    public Module(IMTConnectAgentBroker agent, Sink connector) : base(agent)
-    {
-        _connector = connector;
-    }
-    
-    protected override void OnRead()
-    {
-        _connector.IsWriting = true;
-        
-        foreach (var message in _connector.Outbox)
-        {
-            if (message.ConnectorItemRef is not null &&
-                message.ConnectorItemRef.SinkMeta is not null &&
-                message.ConnectorItemRef.SinkMeta.ContainsKey("mtconnect"))
-            {
-                var devices = Agent.GetDevices().ToDictionary(o => o.Name, o => (Device)o);
-                
-                var (wasModified, device, dataItem) = 
-                    Builder.Build(
-                        devices, 
-                        message.ConnectorItemRef.SinkMeta["mtconnect"].ToString(), 
-                        message.Path);
-
-                //System.Console.WriteLine($"///////// {message.Path} / di={dataItem.Id} / diDevice={dataItem.Device.Uuid} / device={device.Uuid}");
-
-                if (wasModified)
-                {
-                    Agent.AddDevice(device);
-                }
-                
-                Agent.AddObservation(device.Uuid, dataItem.Id, _connector.Configuration.UseSinkTransform ? _connector.TransformAndSerializeMessage(message) : message.Data, message.Timestamp);
-            }
-        }
-        
-        _connector.Outbox.Clear();
-        
-        _connector.IsWriting = false;
     }
 }
