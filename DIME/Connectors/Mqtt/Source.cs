fff9ad9f4e5450549b965e49d7072480f83116da
MRIIOT <cmisztur@mriiot.com>
2024-12-31 19:16:31 -0600

    mqtt perf

--- DIME/Connectors/Mqtt/Source.cs
+++ DIME/Connectors/Mqtt/Source.cs
@@ -1,18 +1,18 @@
-using System.Text;
-using Akka.Actor;
-using Akka.Actor.Internal;
 using DIME.Configuration.Mqtt;
 using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Formatter;
+using MQTTnet.Protocol;
 
 namespace DIME.Connectors.Mqtt;
 
 public class Source: QueuingSourceConnector<ConnectorConfiguration, ConnectorItem>
 {
-    private MQTTnet.Client.IMqttClient _clientMqttNet = null;
-    private TurboMqtt.Client.IMqttClient _clientTurboMqtt = null;
+    private IMqttClient _client = null;
 
     public Source(ConnectorConfiguration configuration, Disruptor.Dsl.Disruptor<MessageBoxMessage> disruptor) : base(configuration, disruptor)
     {
+        
     }
 
     protected override bool InitializeImplementation()
@@ -23,95 +23,40 @@ public class Source: QueuingSourceConnector<ConnectorConfiguration, ConnectorIte
 
     protected override bool CreateImplementation()
     {
-        if (Configuration.UseTurbo)
-        {
-            Logger.Warn($"[{Configuration.Name}] Using TurboMqtt");
-
-           _clientTurboMqtt = new TurboMqtt.Client.MqttClientFactory(new ActorSystemImpl("dime"))
-                .CreateTcpClient(
-                    new TurboMqtt.Client.MqttClientConnectOptions(
-                        Properties.GetProperty<string>("client_id"), 
-                        TurboMqtt.Protocol.MqttProtocolVersion.V3_1_1), 
-                    new TurboMqtt.Client.MqttClientTcpOptions(Configuration.Address, Configuration.Port))
-                .GetAwaiter().GetResult();
-        }
-        else
-        {
-            _clientMqttNet = new MQTTnet.MqttFactory().CreateMqttClient();
-            _clientMqttNet.DisconnectedAsync += MQTTNET_ClientOnDisconnectedAsync;
-            _clientMqttNet.ApplicationMessageReceivedAsync += MQTTNET_ClientOnApplicationMessageReceivedAsync;
-        }
+        _client = new MqttFactory().CreateMqttClient();
+        _client.DisconnectedAsync += ClientOnDisconnectedAsync;
+        _client.ApplicationMessageReceivedAsync += ClientOnApplicationMessageReceivedAsync;
         
         return true;
     }
 
     protected override bool ConnectImplementation()
     {
-        if (Configuration.UseTurbo)
-        {
-            var result = _clientTurboMqtt.ConnectAsync().GetAwaiter().GetResult();
-            
-            foreach(var item in Configuration.Items.Where(x => x.Enabled && !string.IsNullOrEmpty(x.Address)))
-            {
-                _clientTurboMqtt.SubscribeAsync(item.Address, (TurboMqtt.QualityOfService)Configuration.QoS)
-                    .GetAwaiter().GetResult();
-            }
-
-            return result.IsSuccess;
-        }
-        else
-        {
-            var clientOptions = new MQTTnet.Client.MqttClientOptionsBuilder()
-                .WithClientId(Properties.GetProperty<string>("client_id"))
-                .WithTcpServer(Configuration.Address, Configuration.Port)
-                .WithCleanSession(Configuration.CleanSession);
-            var options = clientOptions.Build();
-            var result = _clientMqttNet.ConnectAsync(options).Result;
+        var clientOptions = new MqttClientOptionsBuilder()
+            .WithClientId(Properties.GetProperty<string>("client_id"))
+            .WithTcpServer(Configuration.Address, Configuration.Port)
+            .WithCleanSession(Configuration.CleanSession);
+        var options = clientOptions.Build();
+        var result = _client.ConnectAsync(options).Result;
         
-            var mqttSubscribeOptions = new MQTTnet.MqttFactory().CreateSubscribeOptionsBuilder();
-            foreach(var item in Configuration.Items.Where(x => x.Enabled && !string.IsNullOrEmpty(x.Address)))
+        var mqttSubscribeOptions = new MqttFactory().CreateSubscribeOptionsBuilder();
+        foreach(var item in Configuration.Items.Where(x => x.Enabled && !string.IsNullOrEmpty(x.Address)))
+        {
+            mqttSubscribeOptions.WithTopicFilter(f =>
             {
-                mqttSubscribeOptions.WithTopicFilter(f =>
-                {
-                    f.WithTopic(item.Address);
-                    f.WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)Configuration.QoS);
-                });
-            }
-            var subscribeResult = _clientMqttNet.SubscribeAsync(mqttSubscribeOptions.Build()).Result;
-        
-            return result.ResultCode == MQTTnet.Client.MqttClientConnectResultCode.Success;
+                f.WithTopic(item.Address);
+                f.WithQualityOfServiceLevel((MqttQualityOfServiceLevel)Configuration.QoS);
+            });
         }
+        var subscribeResult = _client.SubscribeAsync(mqttSubscribeOptions.Build()).Result;
+        
+        return result.ResultCode == MqttClientConnectResultCode.Success;;
     }
-
-    protected override bool ReadImplementation()
-    {
-        if (Configuration.UseTurbo)
-        {
-            while (_clientTurboMqtt.ReceivedMessages.TryRead(out var message))
-            {
-                AddToIncomingBuffer(message.Topic, Encoding.UTF8.GetString(message.Payload.Span));
-            }
-        }
-        
-        return base.ReadImplementation();
-    }
-
+    
     protected override bool DisconnectImplementation()
     {
-        if (Configuration.UseTurbo)
-        {
-            _clientTurboMqtt.DisconnectAsync().GetAwaiter().GetResult();
-            return true;
-        }
-        else
-        {
-            _clientMqttNet.DisconnectAsync(
-                new MQTTnet.Client.MqttClientDisconnectOptions()
-                {
-                    Reason = MQTTnet.Client.MqttClientDisconnectOptionsReason.AdministrativeAction
-                }).Wait();
-            return true;
-        }
+        _client.DisconnectAsync().Wait();
+        return true;
     }
     
     protected override bool DeinitializeImplementation()
@@ -119,14 +64,14 @@ public class Source: QueuingSourceConnector<ConnectorConfiguration, ConnectorIte
         return true;
     }
     
-    private Task MQTTNET_ClientOnApplicationMessageReceivedAsync(MQTTnet.Client.MqttApplicationMessageReceivedEventArgs arg)
+    private Task ClientOnApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
     {
         AddToIncomingBuffer(arg.ApplicationMessage.Topic, arg.ApplicationMessage.ConvertPayloadToString());
         
         return Task.FromResult(0);
     }
 
-    private Task MQTTNET_ClientOnDisconnectedAsync(MQTTnet.Client.MqttClientDisconnectedEventArgs arg)
+    private Task ClientOnDisconnectedAsync(MqttClientDisconnectedEventArgs arg)
     {
         IsConnected = false;
         return Task.FromResult(0);
